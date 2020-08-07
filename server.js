8287ff366ab402df265b08c9b40082dd67a076ec
rakesh_rao <rakesh.rao278698@gmail.com>
2020-08-07 14:30:56 +0530

    added flash and session messages

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const Handlebars = require("handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const { connect } = require("mongoose");
 const { PORT, MONGODB_URL } = require("./config");
@@ -15,7 +18,7 @@ connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) throw err;
-    console.log("succesfully connected to database");
+    console.log("successfully connected to database");
   }
 );
 
@@ -34,6 +37,32 @@ Handlebars.registerHelper("removeFirst6Char", (str) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/*========================METHOD OVERRIDE MIDDLEWARE =======================*/
+
+// override with POST having ?_method=PUT or DELETE
+app.use(methodOverride("_method"));
+
+/*==================session and connect flass middlewares are stated here ======*/
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+/*==================session and connect flass middlewares are ended here ======*/
+/*==================SET GLOBAL VARIABLES THIS VARIABLE CAN ACCESS ENTIRE YOUR APPLICATIONS =======*/
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.errors_msg = req.flash("errors_msg");
+  res.locals.warnings_msg = req.flash("warnings_msg ");
+  res.locals.error = req.flash("error");
+
+  next();
+});
+
 /*---------static files----------*/
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules"));
@@ -44,7 +73,7 @@ app.get("/", (req, res) => {
 
 /*------load router files------*/
 app.use("/profile/", require("./Routes/profiles/profile"));
-app.use("/profile/", require("./Routes/auth/auth"));
+app.use("/auth/", require("./Routes/auth/auth"));
 
 app.listen(PORT, (err) => {
   if (err) throw err;
